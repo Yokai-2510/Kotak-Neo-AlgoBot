59f88b36f9e86f262b7c91b55761e3edba458cbc
Eshaan Sharma <153069163+Yokai-2510@users.noreply.github.com>
2024-08-09 02:03:17 +0530

    Update trading_bot.py

--- trading_bot.py
+++ trading_bot.py
@@ -1,61 +1,57 @@
 # trading_bot.py
 
-import threading
-from datetime import datetime, timedelta
-import time
 from config import strategy_dict
-from module_evaluate import start_strategy
-from module_data import fetch_indicator, fetch_spot_yf, run_websocket
-from module_utilities import read_spot_price, parse_time, log_message, clear_log, read_log , display_info
-from credentials import consumer_key, consumer_secret, mobile, mpin, login_password
-from neo_api_client import NeoAPI
-import os
+from module_evaluate import evaluate_strategy
+from module_data import run_websocket
+from module_utilities import log_message, clear_log , display_info
+from credentials import fetch_client
+from module_data import fetch_indicator
 
-# Load strategy dictionary values
+import threading
+from datetime import datetime
+import time
+
+global status_dict
+status_dict = {
+    'order_active': False,
+    'last_zone': None,
+    'current_zone': None,
+    'zone_changed': False,
+    'zone_change_time': None,
+    'current_spot_price': None,
+    'market_status': None ,
+    'current_ltp' : None
+}
+client = None
 index = strategy_dict['index']
 indicator = fetch_indicator(index)
-log_message(indicator)
-last_close = fetch_spot_yf(index)
-current_zone = None
-last_processed_price = None
-client = None
 
-client = NeoAPI(consumer_key=consumer_key, consumer_secret=consumer_secret, environment='prod')
-client.login(mobilenumber=mobile, password=login_password)
-client.session_2fa(OTP=mpin)
+# Connect with Kotak API and store it in client object
+# ------------------------------------------------------------------------------------------------------
+client = fetch_client()
+log_message(f"Client / Access Token fetched successfully")
+# ------------------------------------------------------------------------------------------------------
 
+
+# Start the Websocket Thread
+# ------------------------------------------------------------------------------------------------------
 websocket_thread = threading.Thread(target=run_websocket, args=(client,))
 websocket_thread.start()
-time.sleep(4)
+time.sleep(5)
+# ------------------------------------------------------------------------------------------------------
 
 
-def main():
-    global current_zone, last_processed_price  # Make sure to use global variables
-    
-    market_open_str = strategy_dict.get('market_open', "9:15")
-    market_close_str = strategy_dict.get('market_close', "15:29")
-    
-    market_open = parse_time(market_open_str)
-    market_close = parse_time(market_close_str)
-    
-    while True:
-        time.sleep(1)
-        spot_price = read_spot_price()
+# Start the Evaluation Thread 
+# ------------------------------------------------------------------------------------------------------
+evaluation_thread = threading.Thread(target=evaluate_strategy, args=(client, strategy_dict,status_dict ,))
+evaluation_thread.start()
+time.sleep(1)
+log_message(f"Websocket / Live Data connected successfully")
+# ------------------------------------------------------------------------------------------------------
 
-        current_time = datetime.now().time()
-
-        if current_time > market_close:
-            print("Market closed")
-            break
-
-        if current_time < market_open:
-            print("Waiting for market to open")
-            continue
-
-        current_zone, last_processed_price = start_strategy(client, spot_price, indicator, current_zone, last_processed_price, strategy_dict)
-        
-        display_info(spot_price, current_zone)
 
 if __name__ == "__main__":
     clear_log() 
-    main()
+    while True :
+        display_info(status_dict , indicator)
+        time.sleep(2)
