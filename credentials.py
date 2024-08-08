64abcca265f1cf06172c0daf66b8102dd46bac8d
Eshaan Sharma <153069163+Yokai-2510@users.noreply.github.com>
2024-08-09 02:06:18 +0530

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,6 +1,21 @@
-# credentials
-consumer_key = ''
-consumer_secret = ''
-mobile = ''
-mpin = ''
-login_password = ''
+# credentials.py
+
+
+from neo_api_client import NeoAPI
+
+def fetch_client () :
+
+    # credentials
+    # ------------------------------------------------------------------------------------------------------ 
+    consumer_key = ''
+    consumer_secret = ''
+    mobile = ''
+    mpin = ''
+    login_password = ''
+    # ------------------------------------------------------------------------------------------------------
+
+    client = NeoAPI(consumer_key=consumer_key, consumer_secret=consumer_secret, environment='prod')
+    client.login(mobilenumber=mobile, password=login_password)
+    client.session_2fa(OTP=mpin)
+
+    return client 
