ca8a7289f511593bab4e6f9b8f1722bdf135a640
Eshaan Sharma <153069163+Yokai-2510@users.noreply.github.com>
2024-08-09 07:37:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,15 +4,15 @@ strategy_dict = {
     # User Configuration
     "index" : "NIFTY" , # BANKNIFTY or NIFTY
     'zone_change_delay': 10,  # Delay in seconds between zone changes before order placement
-    "market_open" : "00:15" , # Time at which the Market Opens
+    "market_open" : "9:15" , # Time at which the Market Opens
     "market_close" : "15:29" , # Time at which the Market closes .
 
     # Entry 
     "quantity" : '2', # Enter the Quantity ( in lots)
     "order_type" : "MKT" , # L for LIMIT and MKT for MARKET
     "limit_price" : "0" , # if order type is limit , then put the limit value
-    "ikey_criteria" : "LTP" , # Possible Values : STRIKE , ATM , ITM , LTP
-    "ikey_criteria_value" : "13" , # only applicable for STRIKE and LTP otherwise 0
+    "ikey_criteria" : "ATM" , # Possible Values : STRIKE , ATM , ITM , LTP
+    "ikey_criteria_value" : "0" , # only applicable for STRIKE and LTP otherwise 0
     "AMO" : "False" , # YES  or NO (after market order)
     
     # Exit
